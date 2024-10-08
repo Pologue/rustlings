62bbe3a623270d18c71bbb7d5d69910d3626b025
Pologue <412295017@qq.com>
2024-10-08 17:56:32 +0800

    19_smart_pointers rc1

---
+++ exercises/13_error_handling/errors4.rs
@@ -0,0 +1,46 @@
+#![allow(clippy::comparison_chain)]
+
+#[allow(dead_code)]
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+#[allow(dead_code)]
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+#[allow(dead_code)]
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<Self, CreationError> {
+        // TODO: This function shouldn't always return an `Ok`.
+        match value {
+            0 => Err(CreationError::Zero),
+            x if x < 0 => Err(CreationError::Negative),
+            x => Ok(PositiveNonzeroInteger(x as u64)),
+        }
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_creation() {
+        assert_eq!(
+            PositiveNonzeroInteger::new(10),
+            Ok(PositiveNonzeroInteger(10)),
+        );
+        assert_eq!(
+            PositiveNonzeroInteger::new(-10),
+            Err(CreationError::Negative),
+        );
+        assert_eq!(PositiveNonzeroInteger::new(0), Err(CreationError::Zero));
+    }
+}
